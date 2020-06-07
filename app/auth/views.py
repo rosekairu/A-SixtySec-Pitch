0c8d0e7f0f86faccae20318dedb5c6900adbd07a
Rose Kairu <rosekairu@gmail.com>
2020-06-07 20:26:48 +0300

    modified views file

--- app/auth/views.py
+++ app/auth/views.py
@@ -37,7 +37,7 @@ def register():
         db.session.add(user)
         db.session.commit()
 
-        mail_message("Welcome to SixtySec Pitch","email/welcome_user", user.email, user=user)
+        mail_message("Welcome to SixtySec Pitch","email/welcome", user.email, user=user)
 
         return redirect(url_for('auth.login'))
     title = "SixtySec Pitch New Account"
