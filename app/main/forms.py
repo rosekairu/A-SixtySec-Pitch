cc0b15488355e3bec45d4ecdd03ce47330e1297a
Rose Kairu <rosekairu@gmail.com>
2020-06-07 20:27:45 +0300

    modified forms file in the main folder

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,21 +1,25 @@
 from flask_wtf import FlaskForm
-from wtforms import SelectField,StringField,TextAreaField,SubmitField
-from wtforms.validators import Required
+from wtforms import SelectField,StringField,TextAreaField,RadioField,SubmitField
+from wtforms.validators import Required,Email,EqualTo
 from wtforms import ValidationError
 
 class PitchForm(FlaskForm):
-    '''
-    The pitch-post sharing form
-    '''
-    title = StringField('Pitch Title',validators=[Required()])
-    category = SelectField('', choices=[('TechSavy', 'TechSavy'), ('MoneySmart','MoneySmart'), ('Life & Laughter', 'Life & Laughter')], validators=[Required()])
-    pitch = TextAreaField('', validators=[Required()], render_kw={"placeholder": "Write your story here..."})
-    submit = SubmitField('Share') 
+    title = StringField('Title', validators=[Required()])
+    category = SelectField('Category', choices=[('Events', 'Django'), (
+        'Job', 'Flask'), ('Advertisement', 'Angular')], validators=[Required()])
+    post = TextAreaField('Your Pitch', validators=[Required()])
+    submit = SubmitField('Pitch')
     
 class CommentForm(FlaskForm):
-    comment = TextAreaField('',validators=[Required()], render_kw={"placeholder": "Post your comment here..."})
+    description = TextAreaField('',validators=[Required()], render_kw={"placeholder": "Post your comment here..."})
     submit = SubmitField('Post Comment')
 
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you...',validators = [Required()])
     submit = SubmitField('Submit')
+
+class UpvoteForm(FlaskForm):
+	submit = SubmitField()
+
+class Downvote(FlaskForm):
+	submit = SubmitField()
